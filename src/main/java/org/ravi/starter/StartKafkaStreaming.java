11fac1d994ef92c8411463ab67ee51ab7658c231
ravitechy <71280957+ravitechy@users.noreply.github.com>
2024-06-07 16:49:36 +0530

    Update StartKafkaStreaming.java

--- src/main/java/org/ravi/starter/StartKafkaStreaming.java
+++ src/main/java/org/ravi/starter/StartKafkaStreaming.java
@@ -1,1 +1,22 @@
+package org.ravi.starter;
 
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.ravi.starter.spark.KafkaStreamInitializer;
+import org.ravi.starter.spark.handler.DataStreamHandlerFactory;
+import org.ravi.starter.spark.listener.BatchJobListener;
+
+/**
+ * @author raviteja.kothapalli
+ */
+public class StartKafkaStreaming {
+
+    public static void main(String[] args) throws InterruptedException {
+        JavaDStream<String> dataStream = KafkaStreamInitializer.initDataStream();
+        DataStreamHandlerFactory.INSTANCE.handler.handleStream(dataStream);
+        JavaStreamingContext streamingContext = KafkaStreamInitializer.streamingContext;
+        streamingContext.addStreamingListener(new BatchJobListener());
+        streamingContext.start();
+        streamingContext.awaitTermination();
+    }
+}
