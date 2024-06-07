ab59d1c8ff19c5106b77178f13f0c5a7b4383353
ravitechy <71280957+ravitechy@users.noreply.github.com>
2024-06-07 16:56:56 +0530

    Create SparkKafkaUtil.java

---
+++ src/main/java/org/ravi/starter/spark/util/SparkKafkaUtil.java
@@ -0,0 +1,13 @@
+package org.ravi.starter.spark.util;
+
+/**
+ * @author raviteja.kothapalli
+ */
+public class SparkKafkaUtil {
+
+    private SparkKafkaUtil() {
+
+    }
+
+    public static final String UNIQUE_ID = "uniqueId";
+}
