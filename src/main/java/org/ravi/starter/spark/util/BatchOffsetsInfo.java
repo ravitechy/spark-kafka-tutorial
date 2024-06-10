7f9aa69e5625933276d8a467bd48d9b5b8987d69
ravitechy <71280957+ravitechy@users.noreply.github.com>
2024-06-10 22:41:25 +0530

    Update BatchOffsetsInfo.java

--- src/main/java/org/ravi/starter/spark/util/BatchOffsetsInfo.java
+++ src/main/java/org/ravi/starter/spark/util/BatchOffsetsInfo.java
@@ -5,8 +5,13 @@ package org.ravi.starter.spark.util;
  */
 public class BatchOffsetsInfo {
     public static BatchOffsetsInfo INSTANCE = new BatchOffsetsInfo();
+    private long batchId;
     private long fromOffset;
     private long toOffset;
+
+    public void setBatchId(long batchId) {
+        this.batchId = batchId;
+    }
 
     public void setFromOffset(long fromOffset) {
         System.out.println(Thread.currentThread().getName() + ": Setting the fromOffset value: " + fromOffset);
@@ -26,9 +31,14 @@ public class BatchOffsetsInfo {
         return toOffset;
     }
 
+    public long getBatchId() {
+        return batchId;
+    }
+
     public void reset() {
         System.out.println(Thread.currentThread().getName() + ": Resetting the fromOffSet and toOffset values");
         this.fromOffset = 0;
         this.toOffset = 0;
+        this.batchId = 0;
     }
 }
