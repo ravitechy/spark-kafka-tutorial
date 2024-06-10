80efe986414a7e8f72681cb5ce83ddbe9ee669b1
ravitechy <71280957+ravitechy@users.noreply.github.com>
2024-06-10 22:41:58 +0530

    Update BatchJobListener.java

--- src/main/java/org/ravi/starter/spark/listener/BatchJobListener.java
+++ src/main/java/org/ravi/starter/spark/listener/BatchJobListener.java
@@ -1,9 +1,11 @@
 package org.ravi.starter.spark.listener;
 
+import org.apache.spark.sql.connector.read.Batch;
 import org.apache.spark.streaming.scheduler.BatchInfo;
 import org.apache.spark.streaming.scheduler.OutputOperationInfo;
 import org.apache.spark.streaming.scheduler.StreamingListener;
 import org.apache.spark.streaming.scheduler.StreamingListenerBatchCompleted;
+import org.ravi.starter.spark.util.BatchOffsetsInfo;
 import scala.collection.Iterable;
 
 /**
@@ -15,7 +17,9 @@ public class BatchJobListener implements StreamingListener {
     public void onBatchCompleted(StreamingListenerBatchCompleted batchCompleted) {
         BatchInfo batchInfo = batchCompleted.batchInfo();
         if (batchInfo.numRecords() > 0) {
-            Iterable<OutputOperationInfo> values = batchCompleted.batchInfo().outputOperationInfos().values();
+            System.out.println("Batch Id: " + BatchOffsetsInfo.INSTANCE.getBatchId());
+            System.out.println("Offset range: " + BatchOffsetsInfo.INSTANCE.getFromOffset() + ", " + BatchOffsetsInfo.INSTANCE.getToOffset());
+            BatchOffsetsInfo.INSTANCE.reset();
         }
     }
 }
